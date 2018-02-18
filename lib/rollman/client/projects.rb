1a8ee3ff326c85654c0f790d9aa7495727f19267
grezar <grezar.dev@gmail.com>
2018-02-18 19:39:44 +0900

    Add create_a_project method

--- lib/rollman/client/projects.rb
+++ lib/rollman/client/projects.rb
@@ -8,6 +8,10 @@ module Rollman
       def get_a_project(project_id)
         get "/api/1/project/#{project_id}"
       end
+
+      def create_a_project(project_name, options = {})
+        post '/api/1/projects', options.merge({name: project_name})
+      end
     end
   end
 end
