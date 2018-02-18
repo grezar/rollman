01c014fc34656d0baa4eb4203f98eb15b34563df
grezar <grezar.dev@gmail.com>
2018-02-18 20:13:04 +0900

    Fix to require files in rollman.rb

--- lib/rollman.rb
+++ lib/rollman.rb
@@ -1,4 +1,10 @@
 require "rollman/version"
+require "rollman/client"
+require "rollman/configurable"
+require "rollman/connection"
+require "rollman/client/invites"
+require "rollman/client/teams"
+require "rollman/client/projects"
 
 module Rollman
   # Your code goes here...
