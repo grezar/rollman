9cea04bbb18651310e02ecfd10aa12ae5d8f9b62
grezar <grezar.dev@gmail.com>
2018-02-18 19:43:23 +0900

    Add list_invites_for_a_team method

--- lib/rollman/client/invites.rb
+++ lib/rollman/client/invites.rb
@@ -1,6 +1,10 @@
 module Rollman
   class Client
     module Invites
+      def list_invites_for_a_team(team_id)
+        get "/api/1/team/#{team_id}/invites"
+      end
+
       def invite_a_user_to_a_team(team_id, email)
         post "/api/1/team/#{team_id}/invites", { email: email }
       end
