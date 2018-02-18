4ce3ef85f12d788bf57658796d96132d288b6ccc
grezar <grezar.dev@gmail.com>
2018-02-18 19:34:58 +0900

    Add get_a_project method

--- lib/rollman/client/projects.rb
+++ lib/rollman/client/projects.rb
@@ -4,6 +4,10 @@ module Rollman
       def list_projects
         get '/api/1/projects'
       end
+
+      def get_a_project(project_id)
+        get "/api/1/project/#{project_id}"
+      end
     end
   end
 end
